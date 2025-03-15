f0964ad1a93fefc5e24f7b9c8594d157c868a3ff
petraleumJelly <debatpetra@gmail.com>
2025-03-14 22:49:14 -0400

    Adding Gadget API

---
+++ harold/web/api.ts
@@ -0,0 +1,5 @@
+// Sets up the API client for interacting with your backend.
+// For your API reference, visit: https://docs.gadget.dev/api/harold
+import { Client } from "@gadget-client/harold";
+
+export const api = new Client();
